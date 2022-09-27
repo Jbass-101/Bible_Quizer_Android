9a39168347a462e8eb6b249a8520ac8e400dfc54
Jbass-101 <junior07bj@gmail.com>
2022-09-27 14:37:39 +0200

    Basic theme Added

---
+++ app/src/main/java/com/jbaloji/biblequiz/DatabaseManager.kt
@@ -0,0 +1,13 @@
+package com.jbaloji.biblequiz
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+
+class DatabaseManager {
+
+    val db = Firebase.firestore
+
+
+
+}
--- app/src/main/java/com/jbaloji/biblequiz/SplashScreenActivity.kt
+++ app/src/main/java/com/jbaloji/biblequiz/SplashScreenActivity.kt
@@ -1,16 +1,14 @@
 package com.jbaloji.biblequiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewTreeObserver
-import androidx.lifecycle.ViewModel
 
-class HomeActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
-
+       // startActivity(Intent(this,HomeActivity::class.java))
+      //  finish()
     }
 }
