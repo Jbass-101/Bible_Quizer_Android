d823a1cbb1fa16819cf53433626bae23ba72af65
Jbass-101 <junior07bj@gmail.com>
2022-09-29 21:25:00 +0200

    Fresh

--- app/src/main/java/com/jbaloji/biblequiz/MainActivity.kt
+++ app/src/main/java/com/jbaloji/biblequiz/MainActivity.kt
@@ -1,31 +1,64 @@
 package com.jbaloji.biblequiz
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
-import androidx.core.view.*
-import com.jbaloji.biblequiz.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jbaloji.biblequiz.ui.theme.BibleQuizTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding : ActivityMainBinding
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater);
-        val view = binding.root
-        setContentView(view)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
-
-        hideSystemBars()
+        setContent {
+            BibleQuizTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
     }
+}
 
-    private fun hideSystemBars() {
-        val windowInsetsController =
-            ViewCompat.getWindowInsetsController(window.decorView) ?: return
-        // Configure the behavior of the hidden system bars
-        windowInsetsController.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        // Hide both the status bar and the navigation bar
-        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+@Composable
+fun Greeting(name: String) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+
+            }
+        },
+        bottomBar = {
+            BottomAppBar() {
+
+            }
+        },
+        isFloatingActionButtonDocked = true,
+        floatingActionButtonPosition = FabPosition.End
+    ) {
+
+    }
+}
+
+
+@Preview(
+    name = "light mode",
+    showBackground = true)
+@Preview(
+    name = "Dark Mode",
+    uiMode = UI_MODE_NIGHT_YES,
+)
+@Composable
+fun DefaultPreview() {
+    BibleQuizTheme {
+        Greeting("Android")
     }
 }
