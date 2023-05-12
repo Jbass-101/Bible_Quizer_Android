7ecc8e3d2affebfdb2a6e46244a41171e393c035
Junior Baloji <junior07bj@gmail.com>
2023-05-12 19:34:32 +0200

    changes

--- app/src/main/java/com/jbaloji/biblequiz/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/jbaloji/biblequiz/presentation/home/HomeScreen.kt
@@ -3,14 +3,12 @@ package com.jbaloji.biblequiz.presentation.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
