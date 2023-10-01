90edd07de38de1c34d5730018fb9f297ad1092b4
Junior Baloji <junior07bj@gmail.com>
2023-10-01 23:47:16 +0200

    Updated Constants

---
+++ app/src/main/java/com/jbaloji/biblequiz/core/FirestoreModuleConstants.kt
@@ -0,0 +1,8 @@
+package com.jbaloji.biblequiz.core
+
+object FirestoreModuleConstants {
+    const val Questions = "Questions"
+    const val Levels = "Levels"
+    const val Users = "Users"
+    const val Foundation = "Foundation"
+}
