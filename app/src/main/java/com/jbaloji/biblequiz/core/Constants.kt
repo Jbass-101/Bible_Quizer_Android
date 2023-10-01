75d156cd62d08f4bfc8c92b839540860d239b327
Junior Baloji <junior07bj@gmail.com>
2023-10-01 23:48:15 +0200

    Updated Constants

--- app/src/main/java/com/jbaloji/biblequiz/core/Constants.kt
+++ app/src/main/java/com/jbaloji/biblequiz/core/Constants.kt
@@ -4,26 +4,12 @@ object Constants {
     //App
     const val FILENAME = "Jesus.bq"
     const val TAG = "AppTag"
-    const val DEFAULT_USER = "Default User"
 
-
-    //Navigator
-    const val QUESTION_SCREEN = "questions/{levelId}"
-    const val LEVEL_ID = "levelId"
-    //-----------------------DI--------------------
-    //Questions Module
-    const val ROOT_COLLECTION = "Questions"
-    const val ALL_LEVELS_DOC = "Levels"
-    const val LEVEL_COLLECTION = "Level_1"
-    //User Data Module
 
     //User Data Use Case
     const val TIMED_QUIZ = "Timed Quiz"
     const val SCORE = "Score"
-    const val FIELD_NAME = "level_1"
-    const val UPDATE_VAL = 0
 
     //Api
     const val BASE_URL = "https://extrabrains.co/"
-//    const val BASE_URL = "http://192.168.0.195:3000/"
 }
--- app/src/main/java/com/jbaloji/biblequiz/di/FirestoreModule.kt
+++ app/src/main/java/com/jbaloji/biblequiz/di/FirestoreModule.kt
@@ -1,4 +1,72 @@
 package com.jbaloji.biblequiz.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.jbaloji.biblequiz.core.FirestoreModuleConstants.Foundation
+import com.jbaloji.biblequiz.core.FirestoreModuleConstants.Levels
+import com.jbaloji.biblequiz.core.FirestoreModuleConstants.Questions
+import com.jbaloji.biblequiz.core.FirestoreModuleConstants.Users
+import com.jbaloji.biblequiz.data.repository.LessonRepositoryImpl
+import com.jbaloji.biblequiz.data.repository.QuestionsRepositoryImpl
+import com.jbaloji.biblequiz.data.repository.UserDataRepositoryImpl
+import com.jbaloji.biblequiz.domain.repository.LessonsRepository
+import com.jbaloji.biblequiz.domain.repository.QuestionsRepository
+import com.jbaloji.biblequiz.domain.repository.UserDataRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
+
+@Module
+@InstallIn(SingletonComponent::class)
 object FirestoreModule {
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class QuestionDocRef
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class UsersRef
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class LessonRef
+
+    @QuestionDocRef
+    @Provides
+    fun providesQuestionDocRef(
+        db: FirebaseFirestore
+    ) = db.collection(Questions).document(Levels)
+
+    @UsersRef
+    @Provides
+    fun providesUsersRef(
+        db: FirebaseFirestore
+    ) = db.collection(Users)
+
+    @LessonRef
+    @Provides
+    fun providesLessonRef(
+        db: FirebaseFirestore
+    ) = db.collection(Foundation)
+
+    @Provides
+    fun providesQuestionRepository(
+        @QuestionDocRef docRef: DocumentReference
+    ) : QuestionsRepository = QuestionsRepositoryImpl(docRef)
+
+    @Provides
+    fun providesUserDataRepository(
+        @UsersRef collRef: CollectionReference,
+        authRef: FirebaseAuth
+    ) : UserDataRepository = UserDataRepositoryImpl(collRef,authRef)
+
+    @Provides
+    fun providesLessonRepository(
+        @LessonRef docRef: DocumentReference
+    ) : LessonsRepository = LessonRepositoryImpl(docRef)
 }
